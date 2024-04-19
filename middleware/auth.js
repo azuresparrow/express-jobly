1614b112cabe2f3cb3bdcfaf5e9f12b058eef406
James <jrogers2fc@gmail.com>
2024-04-19 12:37:04 -0700

    change authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -42,8 +42,41 @@ function ensureLoggedIn(req, res, next) {
   }
 }
 
+/** Middleware to test if a user is logged in as an admin
+ * 
+ * If not, raises Unauthorized
+ */
+function ensureAdmin(req, res, next){
+  try {
+    if(!res.locals.user || !res.locals.user.isAdmin) {
+      throw new UnauthorizedError();
+    } 
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+/** Middleware to test if the user matches the current query OR is an admin 
+ * 
+ * If not, raises Unauthorized
+*/
+function ensureCorrectOrAdmin(req, res, next) {
+  try {
+    const targetUsername = req.params.username;
+    const loggedInUser = res.locals.user;
+    if(!(loggedInUser && (targetUsername === loggedInUser.username || loggedInUser.isAdmin))){
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectOrAdmin
 };
--- middleware/auth.test.js
+++ middleware/auth.test.js
@@ -5,6 +5,8 @@ const { UnauthorizedError } = require("../expressError");
 const {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectOrAdmin,
 } = require("./auth");
 
 
